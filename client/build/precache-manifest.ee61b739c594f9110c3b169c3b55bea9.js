039f48d12e72a52587bc05d1a375a4212079979a
Verhana <arrowfoxie@gmail.com>
2018-10-26 21:51:07 -0500

    one more time

---
+++ client/build/precache-manifest.ee61b739c594f9110c3b169c3b55bea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31ba11dc9bd6f87da5fa",
+    "url": "/static/js/main.31ba11dc.chunk.js"
+  },
+  {
+    "revision": "011bd46d81c68bedf310",
+    "url": "/static/js/1.011bd46d.chunk.js"
+  },
+  {
+    "revision": "31ba11dc9bd6f87da5fa",
+    "url": "/static/css/main.f5d841cb.chunk.css"
+  },
+  {
+    "revision": "aebc3bb4ed65e9c1d944994f5872642f",
+    "url": "/index.html"
+  }
+];
